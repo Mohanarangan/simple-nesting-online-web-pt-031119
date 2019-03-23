af6b92e35ac2f3772b7448e4fb0b47a7c48f265b
chandra mohan <mohaniyer_78@hotmail.com>
2019-03-23 09:31:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -87,8 +87,8 @@ def adding_matz
         }
      }
 
-    programmer_hash[:yukihiro_matsumoto][]
-end
+    programmer_hash[:yukihiro_matsumoto] = { known_for: "Ruby" , languages: ["LISP", "C"]
+    end
 
 def changing_alan
 	programmer_hash = 
